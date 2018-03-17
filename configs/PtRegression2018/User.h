f892f80f68e75fbcc9833004ccf33d1076dafff3
Wei Shi <weishi@rice.edu>
2018-03-17 11:20:25 +0100

    Update User.h

--- configs/PtRegression2018/User.h
+++ configs/PtRegression2018/User.h
@@ -2,6 +2,7 @@
 TString OUT_DIR_NAME  = ".";  // Directory for output ROOT file
 TString OUT_FILE_NAME = "PtRegression2018";  // Name base for output ROOT file
 TString EOS_DIR_NAME  = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/ntuples";  // Input directory in 
+TString in_dir = "Ntuples/"; 
 TString SingleMu_files[3] = { 
 	  		    "NTuple_SingleMuon_FlatNtuple_Run_306092_2018_03_02_SingleMu.root",
    			    "NTuple_SingleMuon_FlatNtuple_Run_306135_2018_03_02_SingleMu.root",
@@ -22,6 +23,7 @@ namespace PtRegression2018_cfg {
     
     if (USER == "Wei") {
       EOS_DIR_NAME = "/home/ws13/TMVA/TMVA/INPUT/";  // Input directory in eos
+      in_dir = "Ntuples/"; 
       OUT_DIR_NAME = "/home/ws13/TMVA/TMVA/EMTFPtAssign2018";
       OUT_FILE_NAME = "PtRegression2018";
     }
