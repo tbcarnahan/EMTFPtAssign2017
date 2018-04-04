12e2ea34e29975859fc33ca6da331fccf1086063
Wei Shi <weishi@rice.edu>
2018-04-04 22:29:40 +0200

    Update PtRegression2018.C

--- PtRegression2018.C
+++ PtRegression2018.C
@@ -425,7 +425,7 @@ void PtRegression2018 ( TString myMethodList = "" ) {
 		 }
 	 }//end if 
        }//end Remove bias  
-       if (!isTEST && BarrelRecoMu==0 && EndcapRecoMu<=1 ) isTEST = true;//Don't use EMTF biased event in training
+       if (!isZB && BarrelRecoMu==0 && EndcapRecoMu<=1 ) isTEST = true;//Don't use EMTF biased event in training
        
        if(verbose) std::cout << "End removing bias ... "<< std::endl;    
 	     
