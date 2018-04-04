a38be9ebeb2cd73a883119efa2f1d08b9aa14187
Wei Shi <weishi@rice.edu>
2018-04-04 22:43:03 +0200

    Update PtRegression2018.C

--- PtRegression2018.C
+++ PtRegression2018.C
@@ -452,7 +452,7 @@ void PtRegression2018 ( TString myMethodList = "" ) {
 	 Bool_t mu_train = true;  // tag muon for training 
 	       
 	 //if can't find unique match, assign 1 GeV true pT for training
-	 if( emtf_unique_match == 1 ){//note matched is -999 in 2018 NTuple
+	 if( emtf_unique_match == 1 ){//not uniquely matched is -999 in 2018 NTuple
 		 mu_pt = F("reco_pt", emtf_unique_iMu);
 		 mu_eta = F("reco_eta", emtf_unique_iMu);
 		 mu_phi = F("reco_phi", emtf_unique_iMu);
@@ -460,12 +460,6 @@ void PtRegression2018 ( TString myMethodList = "" ) {
 	 }
 	 else {
 		 continue;
-		 /*
-		 mu_pt = 1;
-		 mu_eta = (ETAMIN+ETAMAX)/2;
-		 mu_phi = emtf_phi;
-	         mu_charge = emtf_charge;
-		 */
 	 }
 	       
 	 if(verbose) std::cout << "RECO kinematics ... "<< std::endl;   
