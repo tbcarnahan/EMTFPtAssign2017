6597b15b17712acefda7ef3a490628c5b050b47e
Wei Shi <weishi@rice.edu>
2018-05-14 19:16:15 +0200

    Update User.h

--- configs/PtRegression2018/User.h
+++ configs/PtRegression2018/User.h
@@ -1,19 +1,14 @@
 // *** Default user settings *** //
 TString EOS_DIR_NAME  = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/ntuples";  // Input directory in 
 TString in_dir = "Ntuples/"; 
-TString SingleMu_files[3] = { 
-	  		    "NTuple_SingleMuon_FlatNtuple_Run_306092_2018_03_02_SingleMu.root",
-   			    "NTuple_SingleMuon_FlatNtuple_Run_306135_2018_03_02_SingleMu.root",
-   			    "NTuple_SingleMuon_FlatNtuple_Run_306154_2018_03_02_SingleMu.root"
+TString SingleMu_files[1] = { 
+   			    "NTuple_SingleMuon_FlatNtuple_Run_306154_2018_05_07_SingleMu_2018_emul_dTh4.root"
 };   
-TString ZeroBias_files[4] = { 
-	  		    "NTuple_ZeroBias1_FlatNtuple_Run_306091_2018_03_02_ZB1.root",
-		            "NTuple_ZeroBias2_FlatNtuple_Run_306091_2018_03_02_ZB2.root",
-		            "NTuple_ZeroBias3_FlatNtuple_Run_306091_2018_03_02_ZB3.root", 
-		 	    "NTuple_ZeroBias4_FlatNtuple_Run_306091_2018_03_02_ZB4.root"
+TString ZeroBias_files[1] = {  
+		 	    "NTuple_ZeroBias1_FlatNtuple_Run_306091_2018_05_07_ZB1_2018_emul_dTh4.root"
 };
-const int USESingleMu = 3;//# of SM files to use
-const int USEZerobias = 4;//# of ZB files to use
+const int USESingleMu = 1;//# of SM files to use
+const int USEZerobias = 1;//# of ZB files to use
 TString OUT_DIR_NAME  = ".";  // Directory for output ROOT file
 TString OUT_FILE_NAME = "PtRegression2018";  // Name base for output ROOT file
 
