d57804729e177dd6a11c3c3286ca38556543ee8e
Matthew <msd14@rice.edu>
2020-11-13 15:02:46 -0600

    Updated efficiency code.

--- eff_L1T.py
+++ eff_L1T.py
@@ -27,8 +27,8 @@ evt_tree.Add(file_name)
 
 
 ## ================ Define histograms ======================
-h_pt_denom = TH1D('h_pt_denom', '', 64, 0., 80.) ; h_pt_numer = TH1D('h_pt_numer', '', 64, 0., 80.)
-h_eta_denom = TH1D('h_eta_denom', '', 32, -3., 3.) ; h_eta_numer = TH1D('h_eta_numer', '', 32, -3., 3.)
+h_pt_denom = TH1D('h_pt_denom', '', 80, 1., 80.) ; h_pt_numer = TH1D('h_pt_numer', '', 80, 1., 80.)
+h_eta_denom = TH1D('h_eta_denom', '', 128, -3., 3.) ; h_eta_numer = TH1D('h_eta_numer', '', 128, -3., 3.)
 h_phi_denom = TH1D('h_phi_denom', '', 64, -180., 180.) ; h_phi_numer = TH1D('h_phi_numer', '', 64, -180., 180.)
 
 
@@ -47,7 +47,7 @@ if printouts==True:
 
 
 ## ================ Event loop ======================
-for iEvt in range(500000):
+for iEvt in range(100000):
 #for iEvt in range(evt_tree.GetEntries()):
   if MAX_EVT > 0 and iEvt > MAX_EVT: break
   if iEvt % PRT_EVT is 0: print 'Event #', iEvt
@@ -55,51 +55,55 @@ for iEvt in range(500000):
   evt_tree.GetEntry(iEvt)
 
   for i in range(len(evt_tree.mu_pt)):
-
     #Fill histogram denominators
-    if abs(evt_tree.mu_eta[i])>1.0 and abs(evt_tree.mu_eta[i])<2.5: h_pt_denom.Fill(evt_tree.mu_pt[i])
-    if evt_tree.mu_pt[i]>20: h_eta_denom.Fill(evt_tree.mu_eta[i])
-    if evt_tree.mu_pt[i]>20: h_phi_denom.Fill(evt_tree.mu_phi[i])
-    
+    if abs(evt_tree.mu_eta[i])>1.0 and abs(evt_tree.mu_eta[i])<2.5:
+      h_pt_denom.Fill(evt_tree.mu_pt[i]) 
+      
+      if evt_tree.mu_pt[i]>20: 
+	h_eta_denom.Fill(evt_tree.mu_eta[i])
+	h_phi_denom.Fill(evt_tree.mu_phi[i])
+
+    #Fill histogram numerators by looking for a Level-1 match. (Same endcap as true muon, ignore duplicated L1 muons using nNeighbor==0)
     for j in range(len(evt_tree.trk_pt)):
+      if evt_tree.trk_pt[j]>20. and abs(evt_tree.mu_eta[i])>1.0 and abs(evt_tree.mu_eta[i])<2.5 and evt_tree.mu_eta[i]*evt_tree.trk_eta[j]>0 and evt_tree.trk_nNeighbor[j]==0:
+	h_pt_numer.Fill(evt_tree.mu_pt[i])
 
-      #Fill histogram numerators by looking for a Level-1 match (Same endcap as true muon, ignore duplicated L1 muons using nNeighbor)
-      if evt_tree.mu_eta[i]*evt_tree.trk_eta[j]>0 and abs(evt_tree.mu_eta[i])>1.0 and abs(evt_tree.mu_eta[i])<2.5:
-	if evt_tree.trk_pt[j]>20. and evt_tree.trk_nNeighbor[j]==0: h_pt_numer.Fill(evt_tree.mu_pt[i])
-	if evt_tree.trk_pt[j]>20. and evt_tree.mu_pt[i]>20. and evt_tree.trk_nNeighbor[j]==0: h_eta_numer.Fill(evt_tree.mu_eta[i])
-	if evt_tree.trk_pt[j]>20. and evt_tree.mu_pt[i]>20. and evt_tree.trk_nNeighbor[j]==0: h_phi_numer.Fill(evt_tree.mu_phi[i])
+	if evt_tree.mu_pt[i]>20.:
+	  h_eta_numer.Fill(evt_tree.mu_eta[i])
+	  h_phi_numer.Fill(evt_tree.mu_phi[i])
 
-    	
+	
 ## ================ Plot/save histograms ======================
+c1 = TCanvas("c1")
+eff = TEfficiency(h_phi_numer, h_phi_denom)
+eff.Draw()
+eff.SetTitle('EMTF Trigger Efficiency vs #phi^{GEN} (p_{T}^{GEN}, p_{T}^{L1} > 20 GeV) (1.3 < #eta^{GEN} < 2.5) ; #phi^{GEN} ; Trigger Effieicny')
+gPad.Update()
+graph = eff.GetPaintedGraph()
+graph.SetMinimum(0)
+graph.SetMaximum(1)
+c1.SaveAs('validation_november/eff_phi_pt20_1pt3eta2pt5.png')
+raw_input("Enter")
 
 c1 = TCanvas("c1")
-h_pt_numer.Divide(h_pt_denom)
-h_pt_numer.SetTitle("Trigger efficiency (p_{T}^{L1} > 20 GeV)")
-h_pt_numer.GetXaxis().SetTitle("p_{T}^{GEN} (GeV)")
-gStyle.SetOptStat(0)
-h_pt_numer.Draw()
+eff = TEfficiency(h_pt_numer, h_pt_denom)
+eff.Draw()
+eff.SetTitle('EMTF Trigger Efficiency vs p_{T}^{GEN} (p_{T}^{L1} > 20 GeV) (1.3 < #eta^{GEN} < 2.5) ; p_{T}^{GEN} ; Trigger Effieicny')
+gPad.Update()
+graph = eff.GetPaintedGraph()
+graph.SetMinimum(0)
+graph.SetMaximum(1)
+c1.SaveAs('validation_november/eff_pt20_1pt3eta2pt5.png')
 raw_input("Enter")
-c1.SaveAs('validation_november/eff_pt20.png')
-c1.Close()
 
 
 c1 = TCanvas("c1")
-h_eta_numer.Divide(h_eta_denom)
-h_eta_numer.SetTitle("Trigger efficiency (p_{T}^{L1}, p_{T}^{GEN} > 20 GeV)")
-h_eta_numer.GetXaxis().SetTitle("#eta")
-gStyle.SetOptStat(0)
-h_eta_numer.Draw()
-raw_input("Enter")
+eff = TEfficiency(h_eta_numer, h_eta_denom)
+eff.Draw()
+eff.SetTitle('EMTF Trigger Efficiency vs #eta^{GEN} (p_{T}^{GEN}, p_{T}^{L1} > 20 GeV) ; #eta^{GEN} ; Trigger Effieicny')
+gPad.Update()
+graph = eff.GetPaintedGraph()
+graph.SetMinimum(0)
+graph.SetMaximum(1)
 c1.SaveAs('validation_november/eff_eta_pt20.png')
-c1.Close()
-
-
-c1 = TCanvas("c1")
-h_phi_numer.Divide(h_phi_denom)
-h_phi_numer.SetTitle("Trigger efficiency (p_{T}^{L1}, p_{T}^{GEN} > 20 GeV)")
-h_phi_numer.GetXaxis().SetTitle("#phi")
-gStyle.SetOptStat(0)
-h_phi_numer.Draw()
 raw_input("Enter")
-c1.SaveAs('validation_november/eff_phi_pt20.png')
-c1.Close()
