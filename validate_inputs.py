aea712267abd4d0c0cd06e1277cb067651ef55a7
Matthew <msd14@rice.edu>
2020-11-17 15:05:48 -0600

    Macro to draw correlation plots.

--- validate_inputs.py
+++ validate_inputs.py
@@ -67,19 +67,13 @@ for iEvt in range(1000000):
   #CLCT can be duplicated at the boundries between sectors. So write a short code that only chooses one phi value per station per endcap.
   ME1_phi_p=0 ; ME2_phi_p=0 ; ME3_phi_p=0 ; ME4_phi_p=0
   ME1_phi_n=0 ; ME2_phi_n=0 ; ME3_phi_n=0 ; ME4_phi_n=0
-  ME1_phi_p_o=0 ; ME2_phi_p_o=0 ; ME3_phi_p_o=0 ; ME4_phi_p_o=0
-  ME1_phi_n_o=0 ; ME2_phi_n_o=0 ; ME3_phi_n_o=0 ; ME4_phi_n_o=0
   ME1_theta_p=0 ; ME2_theta_p=0 ; ME3_theta_p=0 ; ME4_theta_p=0
   ME1_theta_n=0 ; ME2_theta_n=0 ; ME3_theta_n=0 ; ME4_theta_n=0
   for j in range(2):
     for i in range(len(evt_tree.hit_station)):
       if evt_tree.hit_eta[i]*evt_tree.mu_eta[j]>0:# and evt_tree.mu_pt[j]<=15:
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1: h_patterns_ME1.Fill(evt_tree.hit_pattern[i])
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2: h_patterns_ME2.Fill(evt_tree.hit_pattern[i])
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3: h_patterns_ME3.Fill(evt_tree.hit_pattern[i])
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4: h_patterns_ME4.Fill(evt_tree.hit_pattern[i])
-      
-	#if evt_tree.hit_chamber[i]%2==0:
+	
+	#Phi-position values
 	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==1: ME1_phi_p = evt_tree.hit_phi[i]
 	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==-1: ME1_phi_n = evt_tree.hit_phi[i]
 	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2 and evt_tree.hit_endcap[i]==1: ME2_phi_p = evt_tree.hit_phi[i]
@@ -89,62 +83,61 @@ for iEvt in range(1000000):
 	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==1: ME4_phi_p = evt_tree.hit_phi[i]
 	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==-1: ME4_phi_n = evt_tree.hit_phi[i]
 
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==1: ME1_theta_p = evt_tree.hit_theta[i]
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==-1: ME1_theta_n = evt_tree.hit_theta[i]
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2 and evt_tree.hit_endcap[i]==1: ME2_theta_p = evt_tree.hit_theta[i]
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2 and evt_tree.hit_endcap[i]==-1: ME2_theta_n = evt_tree.hit_theta[i]
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3 and evt_tree.hit_endcap[i]==1: ME3_theta_p = evt_tree.hit_theta[i]
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3 and evt_tree.hit_endcap[i]==-1: ME3_theta_n = evt_tree.hit_theta[i]
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==1: ME4_theta_p = evt_tree.hit_theta[i]
-	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==-1: ME4_theta_n = evt_tree.hit_theta[i]
+	
+	'''
+	#Theta-position values
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==1: ME1_theta_p = evt_tree.hit_theta[i]
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==-1: ME1_theta_n = evt_tree.hit_theta[i]
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2 and evt_tree.hit_endcap[i]==1: ME2_theta_p = evt_tree.hit_theta[i]
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2 and evt_tree.hit_endcap[i]==-1: ME2_theta_n = evt_tree.hit_theta[i]
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3 and evt_tree.hit_endcap[i]==1: ME3_theta_p = evt_tree.hit_theta[i]
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3 and evt_tree.hit_endcap[i]==-1: ME3_theta_n = evt_tree.hit_theta[i]
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==1: ME4_theta_p = evt_tree.hit_theta[i]
+	if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==-1: ME4_theta_n = evt_tree.hit_theta[i]
+	
+	#Patterns
+	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1: h_patterns_ME1.Fill(evt_tree.hit_pattern[i])
+	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2: h_patterns_ME2.Fill(evt_tree.hit_pattern[i])
+	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3: h_patterns_ME3.Fill(evt_tree.hit_pattern[i])
+	#if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4: h_patterns_ME4.Fill(evt_tree.hit_pattern[i])
+	'''
 
-	#if evt_tree.hit_chamber[i]%2!=0:
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==1: ME1_phi_p_o = evt_tree.hit_phi[i]
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==1 and evt_tree.hit_endcap[i]==-1: ME1_phi_n_o = evt_tree.hit_phi[i]
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2 and evt_tree.hit_endcap[i]==1: ME2_phi_p_o = evt_tree.hit_phi[i]
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==2 and evt_tree.hit_endcap[i]==-1: ME2_phi_n_o = evt_tree.hit_phi[i]
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3 and evt_tree.hit_endcap[i]==1: ME3_phi_p_o = evt_tree.hit_phi[i]
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==3 and evt_tree.hit_endcap[i]==-1: ME3_phi_n_o = evt_tree.hit_phi[i]
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==1: ME4_phi_p_o = evt_tree.hit_phi[i]
-	  #if evt_tree.hit_isCSC[i]==1 and evt_tree.hit_station[i]==4 and evt_tree.hit_endcap[i]==-1: ME4_phi_n_o = evt_tree.hit_phi[i]
-
-
-    #for i in range(len(evt_tree.trk_mode)):
-      #if evt_tree.trk_eta[i]*evt_tree.mu_eta[j]>0 and evt_tree.mu_pt[j]<=5:
-	#if evt_tree.trk_nNeighbor[i]==0: h_mode.Fill(evt_tree.trk_mode[i])
-
-  #Fill dPhi bendings into 1D histograms.
-  #if ME1_phi_p_e!=0 and ME2_phi_p_e!=0: h_dPhi12.Fill((ME1_phi_p_e - ME2_phi_p_e)*np.pi/180.)
-  #if ME1_phi_n_e!=0 and ME2_phi_n_e!=0: h_dPhi12.Fill((ME1_phi_n_e - ME2_phi_n_e)*np.pi/180.)
-  #if ME2_phi_p_e!=0 and ME3_phi_p_e!=0: h_dPhi23.Fill((ME2_phi_p_e - ME3_phi_p_e)*np.pi/180.)
-  #if ME2_phi_n_e!=0 and ME3_phi_n_e!=0: h_dPhi23.Fill((ME2_phi_n_e - ME3_phi_n_e)*np.pi/180.)
-  #if ME3_phi_p_e!=0 and ME4_phi_p_e!=0: h_dPhi34.Fill((ME3_phi_p_e - ME4_phi_p_e)*np.pi/180.)
-  #if ME3_phi_n_e!=0 and ME4_phi_n_e!=0: h_dPhi34.Fill((ME3_phi_n_e - ME4_phi_n_e)*np.pi/180.)
-
-
+  #2D correlations for phi-position bendings
   if ME1_phi_p!=0 and ME2_phi_p!=0 and ME3_phi_p!=0: h_dPhi12_dPhi23.Fill((ME2_phi_p - ME3_phi_p)*np.pi/180., (ME1_phi_p - ME2_phi_p)*np.pi/180.)
   if ME1_phi_n!=0 and ME2_phi_n!=0 and ME3_phi_n!=0: h_dPhi12_dPhi23.Fill((ME2_phi_n - ME3_phi_n)*np.pi/180., (ME1_phi_n - ME2_phi_n)*np.pi/180.)
   if ME2_phi_p!=0 and ME3_phi_p!=0 and ME4_phi_p!=0: h_dPhi23_dPhi34.Fill((ME3_phi_p - ME4_phi_p)*np.pi/180., (ME2_phi_p - ME3_phi_p)*np.pi/180.)
   if ME2_phi_n!=0 and ME3_phi_n!=0 and ME4_phi_n!=0: h_dPhi23_dPhi34.Fill((ME3_phi_n - ME4_phi_n)*np.pi/180., (ME2_phi_n - ME3_phi_n)*np.pi/180.)
   if ME1_phi_p!=0 and ME2_phi_p!=0 and ME3_phi_p!=0 and ME4_phi_p!=0: h_dPhi12_dPhi34.Fill((ME3_phi_p - ME4_phi_p)*np.pi/180., (ME1_phi_p - ME2_phi_p)*np.pi/180.)
   if ME1_phi_n!=0 and ME2_phi_n!=0 and ME3_phi_n!=0 and ME4_phi_n!=0: h_dPhi12_dPhi34.Fill((ME3_phi_n - ME4_phi_n)*np.pi/180., (ME1_phi_n - ME2_phi_n)*np.pi/180.)
-  
-  ##Fill dTheta bendings into 1D histograms.
-  #if ME1_theta_p!=0 and ME2_theta_p!=0: h_dTheta12.Fill((ME1_theta_p - ME2_theta_p)*np.pi/180.)
-  #if ME1_theta_n!=0 and ME2_theta_n!=0: h_dTheta12.Fill((ME1_theta_n - ME2_theta_n)*np.pi/180.)
-  #if ME2_theta_p!=0 and ME3_theta_p!=0: h_dTheta23.Fill((ME2_theta_p - ME3_theta_p)*np.pi/180.)
-  #if ME2_theta_n!=0 and ME3_theta_n!=0: h_dTheta23.Fill((ME2_theta_n - ME3_theta_n)*np.pi/180.)
-  #if ME3_theta_p!=0 and ME4_theta_p!=0: h_dTheta34.Fill((ME3_theta_p - ME4_theta_p)*np.pi/180.)
-  #if ME3_theta_n!=0 and ME4_theta_n!=0: h_dTheta34.Fill((ME3_theta_n - ME4_theta_n)*np.pi/180.)
-  
 
   '''
+    #for i in range(len(evt_tree.trk_mode)):
+      #if evt_tree.trk_eta[i]*evt_tree.mu_eta[j]>0 and evt_tree.mu_pt[j]<=5:
+	#if evt_tree.trk_nNeighbor[i]==0: h_mode.Fill(evt_tree.trk_mode[i])
+
+  Fill dPhi bendings into 1D histograms.
+  if ME1_phi_p_e!=0 and ME2_phi_p_e!=0: h_dPhi12.Fill((ME1_phi_p_e - ME2_phi_p_e)*np.pi/180.)
+  if ME1_phi_n_e!=0 and ME2_phi_n_e!=0: h_dPhi12.Fill((ME1_phi_n_e - ME2_phi_n_e)*np.pi/180.)
+  if ME2_phi_p_e!=0 and ME3_phi_p_e!=0: h_dPhi23.Fill((ME2_phi_p_e - ME3_phi_p_e)*np.pi/180.)
+  if ME2_phi_n_e!=0 and ME3_phi_n_e!=0: h_dPhi23.Fill((ME2_phi_n_e - ME3_phi_n_e)*np.pi/180.)
+  if ME3_phi_p_e!=0 and ME4_phi_p_e!=0: h_dPhi34.Fill((ME3_phi_p_e - ME4_phi_p_e)*np.pi/180.)
+  if ME3_phi_n_e!=0 and ME4_phi_n_e!=0: h_dPhi34.Fill((ME3_phi_n_e - ME4_phi_n_e)*np.pi/180.)
+
+   
+  #dTheta bendings 1D histograms.
+  if ME1_theta_p!=0 and ME2_theta_p!=0: h_dTheta12.Fill((ME1_theta_p - ME2_theta_p)*np.pi/180.)
+  if ME1_theta_n!=0 and ME2_theta_n!=0: h_dTheta12.Fill((ME1_theta_n - ME2_theta_n)*np.pi/180.)
+  if ME2_theta_p!=0 and ME3_theta_p!=0: h_dTheta23.Fill((ME2_theta_p - ME3_theta_p)*np.pi/180.)
+  if ME2_theta_n!=0 and ME3_theta_n!=0: h_dTheta23.Fill((ME2_theta_n - ME3_theta_n)*np.pi/180.)
+  if ME3_theta_p!=0 and ME4_theta_p!=0: h_dTheta34.Fill((ME3_theta_p - ME4_theta_p)*np.pi/180.)
+  if ME3_theta_n!=0 and ME4_theta_n!=0: h_dTheta34.Fill((ME3_theta_n - ME4_theta_n)*np.pi/180.)
+  
   #Pick out pT of muons in (+) and (-) endcap
   for i in range(len(evt_tree.mu_pt)):
     if evt_tree.mu_eta[i]>0 and evt_tree.mu_pt[i]<=15: gen_pt_p = evt_tree.mu_pt[i]
     if evt_tree.mu_eta[i]<0 and evt_tree.mu_pt[i]<=15: gen_pt_n = evt_tree.mu_pt[i]
 
-  #Fill Bending vs pT 2D histograms.
+  #Phi-position bending vs pT 2D histograms.
   if ME1_phi_p!=0 and ME2_phi_p!=0: h_dPhi12_pt.Fill(gen_pt_p, (ME1_phi_p - ME2_phi_p)*np.pi/180.)
   if ME1_phi_n!=0 and ME2_phi_n!=0: h_dPhi12_pt.Fill(gen_pt_n, (ME1_phi_n - ME2_phi_n)*np.pi/180.)
   if ME2_phi_p!=0 and ME3_phi_p!=0: h_dPhi23_pt.Fill(gen_pt_p, (ME2_phi_p - ME3_phi_p)*np.pi/180.)
@@ -152,6 +145,7 @@ for iEvt in range(1000000):
   if ME3_phi_p!=0 and ME4_phi_p!=0: h_dPhi34_pt.Fill(gen_pt_p, (ME3_phi_p - ME4_phi_p)*np.pi/180.)
   if ME3_phi_n!=0 and ME4_phi_n!=0: h_dPhi34_pt.Fill(gen_pt_n, (ME3_phi_n - ME4_phi_n)*np.pi/180.)
   
+  #Theta-position bending vs pT 2D histogram
   if ME1_theta_p!=0 and ME2_theta_p!=0: h_dTheta12_pt.Fill(gen_pt_p, (ME1_theta_p - ME2_theta_p)*np.pi/180.)
   if ME1_theta_n!=0 and ME2_theta_n!=0: h_dTheta12_pt.Fill(gen_pt_n, (ME1_theta_n - ME2_theta_n)*np.pi/180.)
   if ME2_theta_p!=0 and ME3_theta_p!=0: h_dTheta23_pt.Fill(gen_pt_p, (ME2_theta_p - ME3_theta_p)*np.pi/180.)
@@ -159,6 +153,41 @@ for iEvt in range(1000000):
   if ME3_theta_p!=0 and ME4_theta_p!=0: h_dTheta34_pt.Fill(gen_pt_p, (ME3_theta_p - ME4_theta_p)*np.pi/180.)
   if ME3_theta_n!=0 and ME4_theta_n!=0: h_dTheta34_pt.Fill(gen_pt_n, (ME3_theta_n - ME4_theta_n)*np.pi/180.)
   '''
+
+#2D correlations
+c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
+h_dPhi12_dPhi23.Draw("colz")
+gStyle.SetOptStat(0)
+gPad.SetLogz()
+h_dPhi12_dPhi23.SetTitle('dPhi_{12} vs dPhi_{23}')
+h_dPhi12_dPhi23.GetXaxis().SetTitle('dPhi_{23} (radians)') ; h_dPhi12_dPhi23.GetYaxis().SetTitle('dPhi_{12} (radians)')
+h_dPhi12_dPhi23.Write()
+c1.SaveAs('validation_november/dPhi12_dPhi23.png')
+#raw_input("Enter")
+c1.Close()
+
+c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
+h_dPhi12_dPhi34.Draw("colz")
+gStyle.SetOptStat(0)
+gPad.SetLogz()
+h_dPhi12_dPhi34.SetTitle('dPhi_{12} vs dPhi_{34}')
+h_dPhi12_dPhi34.GetXaxis().SetTitle('dPhi_{34} (radians)') ; h_dPhi12_dPhi34.GetYaxis().SetTitle('dPhi_{12} (radians)')
+h_dPhi12_dPhi34.Write()
+c1.SaveAs('validation_november/dPhi12_dPhi34.png')
+#raw_input("Enter")
+c1.Close()
+
+c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
+h_dPhi23_dPhi34.Draw("colz")
+gStyle.SetOptStat(0)
+h_dPhi23_dPhi34.SetTitle('dPhi_{23} vs dPhi_{34}')
+h_dPhi23_dPhi34.GetXaxis().SetTitle('dPhi_{34} (radians)') ; h_dPhi23_dPhi34.GetYaxis().SetTitle('dPhi_{23} (radians)')
+h_dPhi23_dPhi34.Write()
+gPad.SetLogz()
+c1.SaveAs('validation_november/dPhi23_dPhi34.png')
+#raw_input("Enter")
+c1.Close()
+
 
 '''
 c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
@@ -220,41 +249,7 @@ h_dTheta34_pt.SetOption("colz")
 h_dTheta34_pt.Write()
 c1.SaveAs('validation_november/dTheta34_pt.png')
 #raw_input("Enter")
-'''
 
-
-c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
-h_dPhi12_dPhi23.Draw("colz")
-gStyle.SetOptStat(0)
-gPad.SetLogz()
-h_dPhi12_dPhi23.SetTitle('dPhi_{12} vs dPhi_{23}')
-h_dPhi12_dPhi23.GetXaxis().SetTitle('dPhi_{23} (radians)') ; h_dPhi12_dPhi23.GetYaxis().SetTitle('dPhi_{12} (radians)')
-h_dPhi12_dPhi23.Write()
-c1.SaveAs('validation_november/dPhi12_dPhi23.png')
-raw_input("Enter")
-c1.Close()
-
-c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
-h_dPhi12_dPhi34.Draw("colz")
-gStyle.SetOptStat(0)
-gPad.SetLogz()
-h_dPhi12_dPhi34.SetTitle('dPhi_{12} vs dPhi_{34}')
-h_dPhi12_dPhi34.GetXaxis().SetTitle('dPhi_{34} (radians)') ; h_dPhi12_dPhi34.GetYaxis().SetTitle('dPhi_{12} (radians)')
-h_dPhi12_dPhi34.Write()
-c1.SaveAs('validation_november/dPhi12_dPhi34.png')
-raw_input("Enter")
-c1.Close()
-
-c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
-h_dPhi23_dPhi34.Draw("colz")
-gStyle.SetOptStat(0)
-h_dPhi23_dPhi34.SetTitle('dPhi_{23} vs dPhi_{34}')
-h_dPhi23_dPhi34.GetXaxis().SetTitle('dPhi_{34} (radians)') ; h_dPhi23_dPhi34.GetYaxis().SetTitle('dPhi_{23} (radians)')
-h_dPhi23_dPhi34.Write()
-gPad.SetLogz()
-c1.SaveAs('validation_november/dPhi23_dPhi34.png')
-raw_input("Enter")
-c1.Close()
 
 
 #c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
@@ -279,7 +274,6 @@ c1.Close()
 #c1.SaveAs('validation_november/dPhi34_ee_pt15.png')
 ##raw_input("Enter")
 
-
 #c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
 #h_dTheta12.Draw()
 #h_dTheta12.SetTitle('dTheta_{12} (radians)')
@@ -298,7 +292,6 @@ c1.Close()
 #c1.SaveAs('validation_november/dTheta34.png')
 ##raw_input("Enter")
 
-'''
 c1 = TCanvas( 'c1', '', 200, 10, 700, 500)
 h_mode.Draw()
 h_mode.SetTitle('Track mode occupancy (p_{T}^{GEN} < 5 GeV)')
