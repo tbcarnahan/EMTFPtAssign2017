a0cf9fa451ce7d7df47c35715c0f23269d69332d
Matthew <msd14@rice.edu>
2020-12-15 12:50:57 -0600

    Condor configuration files for BDT training.

--- condor/condor_BDT.sh
+++ condor/condor_BDT.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-xrdcp -s root://cmseos.fnal.gov//store/user/mdecaro/file.tar .
+
+#You'll need to first compress your CMSSW release (with BDT code) into
+#  a tarball and xrdcp it into eos. Point the next line to your tarball.
+xrdcp -s root://cmseos.fnal.gov//store/user/mdecaro/EMTFPtAssign2017.tar .
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-tar -xf file.tar
-rm file.tar
+tar -xf EMTFPtAssign2017.tar
+rm EMTFPtAssign2017.tar
 cd CMSSW_10_6_1_patch2/src/
 scramv1 b ProjectRename
 eval `scramv1 runtime -sh`
@@ -13,5 +16,6 @@ pwd
 cd EMTFPtAssign2017
 root -l -b PtRegression2019_GEM_SD.C
 
+#Specify the BDT output to a location on eos.
 xrdcp -f Pt*.root root://cmseos.fnal.gov//store/user/mdecaro/condor_output_BDT/
 rm Pt*.root
