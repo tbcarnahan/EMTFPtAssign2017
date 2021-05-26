a3bb969090480fbe255b0b2e98454c262bdb99d7
Sven Dildick <sven.dildick@cern.ch>
2021-05-26 13:40:04 -0500

    working efficiency plots

--- macros_Rice2020/efficiencyPlots.py
+++ macros_Rice2020/efficiencyPlots.py
@@ -1,29 +1,191 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import os
-import math
-import numpy as np
-from array import *
 from termcolor import colored
-from optparse import OptionParser,OptionGroup
+from tdrstyle import *
+from CMS_lumi import *
+from helpers import *
+from ROOT import *
+
+
+def efficiencyVsPt_PtCutEtaSlice(emtfMode, plotter, ptCutIndex, etaSliceIndex):
+  ptCut = plotter.options.emtfPtCuts[ptCutIndex]
+  etaMin = plotter.options.etaMins[etaSliceIndex]
+  etaMax = plotter.options.etaMaxs[etaSliceIndex]
+  etaMinStr = str(etaMin).replace('.','p')
+  etaMaxStr = str(etaMax).replace('.','p')
+
+  if plotter.options.verbosity >= 3:
+    print(colored("\tpT cut: {}, eta_min: {}, eta_max: {}".format(ptCut, etaMin, etaMax), 'green'))
+
+  c1 = newCanvas()
+  leg = TLegend(0.45, 0.23, 0.90, 0.50, "", "brNDC")
+  leg.SetHeader("p_{T}^{GEN}, p_{T}^{L1} #geq %d GeV"%ptCut)
+  leg.SetBorderSize(0)
+  leg.SetFillStyle(0)
+  leg.SetFillColor(0)
+  leg.SetTextSize(0.04)
+  gStyle.SetOptStat(0)
+
+  #Run2 and Run3 BDT efficiency vs Pt
+  effs = []
+  for ee in range(0,len(plotter.eventTrees)):
+    eff = draw_eff(plotter.eventTrees[ee], "; p_{T}^{GEN} (GeV) ; Trigger Efficiency", "(50,1.,50.)", "GEN_pt",
+                   gen_eta_cut(etaMin, etaMax),
+                   bdt_pt_scaled_Run3(ptCut))
+    eff.SetMarkerColor(plotter.emtfColors[ee])
+    eff.SetLineColor(plotter.emtfColors[ee])
+    eff.SetMarkerStyle(8)
+    eff.Draw(plotter.emtfDrawOptions[ee])
+    effs.append(eff)
+    leg.AddEntry(effs[ee], plotter.legendEntries[ee], "pl")
+  eff.Draw("same")
+
+  gPad.Update()
+  graph = eff.GetPaintedGraph()
+  graph.SetMinimum(0)
+  graph.SetMaximum(1.1)
+  graph.GetXaxis().SetLabelSize(0.05)
+  graph.GetYaxis().SetLabelSize(0.05)
+  graph.GetXaxis().SetTitleSize(0.05)
+  graph.GetYaxis().SetTitleSize(0.05)
+  gPad.Update()
+
+  leg.Draw("same")
+
+  line = TLine(0, 0.9, 50, 0.9)
+  line2 = TLine(ptCut, 0., ptCut, 1.1)
+  line.SetLineStyle(7)
+  line2.SetLineStyle(7)
+  line.Draw("same")
+  line2.Draw("same")
+
+  c1.Modified()
+  c1.Update()
+  CMS_lumi.CMS_lumi(c1, plotter.iPeriod, plotter.iPos)
+
+  makePlots(c1, plotter.plotDir, "BDT_eff_pt_mode{}_pt{}_eta{}to{}".format(emtfMode, ptCut, etaMinStr, etaMaxStr))
+
+
+def efficiencyVsEta_PtCut(emtfMode, plotter, ptCutIndex):
+  ptCut = plotter.options.emtfPtCuts[ptCutIndex]
+
+  if plotter.options.verbosity >= 3:
+    print(colored("\tpT cut: {}".format(ptCut), 'green'))
+
+  #Run2 and Run3 BDT efficiency vs Eta
+  binning = "(64,-3.,3.)"
+  effs = []
+
+  c1 = newCanvas()
+  for ee in range(0,len(plotter.eventTrees)):
+    eff = draw_eff(plotter.eventTrees[ee], " ; #eta^{GEN} ; Trigger Efficiency", binning, "GEN_eta", gen_pt_cut(ptCut), bdt_pt_scaled_Run3(ptCut))
+    eff.SetMarkerColor(plotter.emtfColors[ee])
+    eff.SetLineColor(plotter.emtfColors[ee])
+    eff.SetMarkerStyle(8)
+    eff.Draw(plotter.emtfDrawOptions[ee])
+    effs.append(eff)
+
+  leg = TLegend(0.35, 0.2, 0.6, 0.5);
+  leg.SetHeader("p_{T}^{GEN}, p_{T}^{L1} #geq %d GeV"%ptCut)
+  leg.SetBorderSize(0)
+  leg.SetFillStyle(0)
+  leg.SetFillColor(0)
+  leg.SetTextSize(0.04)
+
+  gPad.Update()
+  gStyle.SetOptStat(0)
+  graph = eff.GetPaintedGraph()
+  graph.SetMinimum(0)
+  graph.SetMaximum(1.1)
+  graph.GetXaxis().SetLabelSize(0.05)
+  graph.GetYaxis().SetLabelSize(0.05)
+  graph.GetXaxis().SetTitleSize(0.05)
+  graph.GetYaxis().SetTitleSize(0.05)
+
+  leg.Draw("same")
+
+  c1.Modified()
+  c1.Update()
+  CMS_lumi.CMS_lumi(c1, plotter.iPeriod, plotter.iPos)
+
+  makePlots(c1, plotter.plotDir, "BDT_eff_eta_mode{}_pt{}".format(emtfMode, ptCut))
+
+
+def efficiencyVsPhi_PtCut(emtfMode, plotter, ptCutIndex):
+  ptCut = plotter.options.emtfPtCuts[ptCutIndex]
+  etaMin = 1.25
+  etaMax = 2.4
+  etaMinStr = str(etaMin).replace('.','p')
+  etaMaxStr = str(etaMax).replace('.','p')
+
+  if plotter.options.verbosity >= 3:
+    print(colored("\tpT cut: {}, eta_min: {}, eta_max: {}".format(ptCut, etaMin, etaMax), 'green'))
+
+  #Run2 and Run3 BDT efficiency vs Phi
+  binning = "(64,-3.,3.)"
+  effs = []
+
+  c1 = newCanvas()
+  for ee in range(0,len(plotter.eventTrees)):
+    eff = draw_eff(plotter.eventTrees[ee], " ; #phi^{GEN} ; Trigger Efficiency",
+                   binning, "GEN_phi", gen_pt_cut(ptCut), bdt_pt_scaled_Run3(ptCut))
+    eff.SetMarkerColor(plotter.emtfColors[ee])
+    eff.SetLineColor(plotter.emtfColors[ee])
+    eff.SetMarkerStyle(8)
+    eff.Draw(plotter.emtfDrawOptions[ee])
+    effs.append(eff)
+
+  leg = TLegend(0.35, 0.2, 0.6, 0.5);
+  leg.SetHeader("p_{T}^{GEN}, p_{T}^{L1} #geq %d GeV"%ptCut)
+  leg.SetBorderSize(0)
+  leg.SetFillStyle(0)
+  leg.SetFillColor(0)
+  leg.SetTextSize(0.04)
+
+  gPad.Update()
+  gStyle.SetOptStat(0)
+  graph = eff.GetPaintedGraph()
+  graph.SetMinimum(0)
+  graph.SetMaximum(1.1)
+  graph.GetXaxis().SetLabelSize(0.05)
+  graph.GetYaxis().SetLabelSize(0.05)
+  graph.GetXaxis().SetTitleSize(0.05)
+  graph.GetYaxis().SetTitleSize(0.05)
+
+  leg.Draw("same")
+
+  c1.Modified()
+  c1.Update()
+  CMS_lumi.CMS_lumi(c1, plotter.iPeriod, plotter.iPos)
+
+  makePlots(c1, plotter.plotDir, "BDT_eff_phi_mode{}_pt{}".format(emtfMode, ptCut))
+
 
 def efficiencyVsPt(plotter):
-  print(colored("Producing efficiency vs pT plots for mode {}".format(plotter.emtfMode), 'green'))
-  pass
+  if plotter.options.verbosity >= 2:
+    print(colored("Producing efficiency vs pT plots for mode {}".format(plotter.emtfMode), 'green'))
 
-def efficiencyVsPt(plotter):
-  print(colored("Producing efficiency vs pT plots for mode {}".format(plotter.emtfMode), 'green'))
-  pass
+  for l in range(0,len(plotter.options.emtfPtCuts)):
+    for k in range(0,len(plotter.options.etaMins)):
+      efficiencyVsPt_PtCutEtaSlice(plotter.emtfMode, plotter, l, k)
+
 
 def efficiencyVsEta(plotter):
-  print(colored("Producing efficiency vs eta plots for mode {}".format(plotter.emtfMode), 'green'))
-  pass
+  if plotter.options.verbosity >= 2:
+    print(colored("Producing efficiency vs eta plots for mode {}".format(plotter.emtfMode), 'green'))
+
+  for l in range(0,len(plotter.options.emtfPtCuts)):
+    efficiencyVsEta_PtCut(plotter.emtfMode, plotter, l)
+
 
 def efficiencyVsPhi(plotter):
-  print(colored("Producing efficiency vs phi plots for mode {}".format(plotter.emtfMode), 'green'))
-  pass
+  if plotter.options.verbosity >= 2:
+    print(colored("Producing efficiency vs phi plots for mode {}".format(plotter.emtfMode), 'green'))
+
+  for l in range(0,len(plotter.options.emtfPtCuts)):
+    efficiencyVsPhi_PtCut(plotter.emtfMode, plotter, l)
+
 
 def plotEfficienciesSingleMode(plotter):
 
@@ -35,148 +197,3 @@ def plotEfficienciesSingleMode(plotter):
 
   if plotter.options.effVsPhi:
     efficiencyVsPhi(plotter)
-
-"""
-  for l in range(len(pt_cut)):
-    for k in range(len(eta_min)):
-
-      c1 = newCanvas()
-
-      leg = TLegend(0.45, 0.23, 0.90, 0.50, "", "brNDC")
-      leg.SetBorderSize(0)
-      leg.SetFillStyle(0)
-      leg.SetFillColor(0)
-      leg.SetTextSize(0.04)
-      gStyle.SetOptStat(0)
-
-      #Run2 and Run3 BDT efficiency vs Pt
-      effs = []
-      for ee in range(0,len(trainings)):
-        eff = draw_eff(evt_trees[ee], "; p_{T}^{GEN} (GeV) ; Trigger Efficiency", "(50,1.,50.)", "GEN_pt",
-                       gen_eta_cut(eta_min[k], eta_max[k]), bdt_pt_scaled_Run3(pt_cut[l]))
-        eff.SetMarkerColor(markerColors[ee])
-        eff.SetLineColor(lineColors[ee])
-        eff.SetMarkerStyle(markerStyles[ee])
-        eff.Draw(drawOptions[ee])
-        effs.append(eff)
-
-        leg.AddEntry(effs[ee], legendEntries[ee], "pl")
-        eff.Draw("same")
-
-        gPad.Update()
-        graph = eff.GetPaintedGraph()
-        graph.SetMinimum(0)
-        graph.SetMaximum(1.1)
-        graph.GetXaxis().SetLabelSize(0.05)
-        graph.GetYaxis().SetLabelSize(0.05)
-        graph.GetXaxis().SetTitleSize(0.05)
-        graph.GetYaxis().SetTitleSize(0.05)
-        gPad.Update()
-
-      leg.Draw("same")
-
-      line = TLine(0, 0.9, 50, 0.9)
-      line2 = TLine(pt_cut[l], 0., pt_cut[l], 1.1)
-      line.SetLineStyle(7)
-      line2.SetLineStyle(7)
-      line.Draw("same")
-      line2.Draw("same")
-
-      tex = TLatex()
-      tex.SetTextColor(kBlack)
-      #tex.SetTextFont(22)
-      tex.SetTextSize(0.05)
-      tex.DrawLatex( 35, 0.09, "p_{T}^{L1} > "+str(int(pt_cut[l]))+" GeV")
-
-      c1.Modified()
-      c1.Update()
-      CMS_lumi.CMS_lumi(c1, iPeriod, iPos)
-
-      makePlots(c1, plotDir, "BDT_eff_SD_pt{}_eta{}to{}".format(pt_str[l], eta_str_min[k], eta_str_max[k]))
-
-
-if options.effVsEta:
-
-  for l in range(len(pt_cut)):
-    #Run2 and Run3 BDT efficiency vs Eta
-    binning = "(64,-3.,3.)"
-    effs = []
-
-    c1 = newCanvas()
-    for ee in range(0,len(trainings)):
-      eff = draw_eff(evt_trees[ee], " ; #eta^{GEN} ; Trigger Efficiency", binning, "GEN_eta", gen_pt_cut(pt_cut[l]), bdt_pt_scaled_Run3(pt_cut[l]))
-      eff.SetMarkerColor(markerColors[ee])
-      eff.SetLineColor(lineColors[ee])
-      eff.SetMarkerStyle(markerStyles[ee])
-      eff.Draw(drawOptions[ee])
-      effs.append(eff)
-
-    leg = TLegend(0.35, 0.2, 0.6, 0.5);
-    leg.SetHeader("p_{T}^{GEN}, p_{T}^{L1} > "+str(pt_cut[l])+" GeV")
-    leg.SetBorderSize(0)
-    leg.SetFillStyle(0)
-    leg.SetFillColor(0)
-    leg.SetTextSize(0.04)
-    for ee in range(0,len(trainings)):
-      leg.AddEntry(effs[ee], legendEntries[ee], "pl")
-      leg.Draw("same")
-
-    gPad.Update()
-    gStyle.SetOptStat(0)
-    graph = eff.GetPaintedGraph()
-    graph.SetMinimum(0)
-    graph.SetMaximum(1.1)
-    graph.GetXaxis().SetLabelSize(0.05)
-    graph.GetYaxis().SetLabelSize(0.05)
-    graph.GetXaxis().SetTitleSize(0.05)
-    graph.GetYaxis().SetTitleSize(0.05)
-
-    c1.Modified()
-    c1.Update()
-    CMS_lumi.CMS_lumi(c1, iPeriod, iPos)
-
-    makePlots(c1, plotDir, "BDTeff_eta_pt"+str(pt_str[l]) )
-
-
-if options.effVsPhi:
-
-  for l in range(len(pt_cut)):
-    #Run2 and Run3 BDT efficiency vs Phi
-    binning = "(64,-3.2,3.2)"
-    effs = []
-
-    c1 = newCanvas()
-    for ee in range(0,len(trainings)):
-      eff = draw_eff(evt_trees[ee], " ; #phi^{GEN} ; Trigger Efficiency", binning, "GEN_phi", gen_pt_cut(pt_cut[l]), bdt_pt_scaled_Run3(pt_cut[l]))
-      eff.SetMarkerColor(markerColors[ee])
-      eff.SetLineColor(lineColors[ee])
-      eff.SetMarkerStyle(markerStyles[ee])
-      eff.Draw(drawOptions[ee])
-      effs.append(eff)
-
-    leg = TLegend(0.35, 0.2, 0.6, 0.5);
-    leg.SetHeader("p_{T}^{GEN}, p_{T}^{L1} > "+str(pt_cut[l])+" GeV")
-    leg.SetBorderSize(0)
-    leg.SetFillStyle(0)
-    leg.SetFillColor(0)
-    leg.SetTextSize(0.04)
-    for ee in range(0,len(trainings)):
-      leg.AddEntry(effs[ee], legendEntries[ee], "pl")
-      leg.Draw("same")
-
-    gPad.Update()
-    gStyle.SetOptStat(0)
-    graph = eff.GetPaintedGraph()
-    graph.SetMinimum(0)
-    graph.SetMaximum(1.1)
-    graph.GetXaxis().SetLabelSize(0.05)
-    graph.GetYaxis().SetLabelSize(0.05)
-    graph.GetXaxis().SetTitleSize(0.05)
-    graph.GetYaxis().SetTitleSize(0.05)
-
-    c1.Modified()
-    c1.Update()
-    CMS_lumi.CMS_lumi(c1, iPeriod, iPos)
-
-    makePlots(c1, plotDir, "BDTeff_phi_pt"+str(pt_str[l]) )
-"""
--- macros_Rice2020/makePerformancePlots.py
+++ macros_Rice2020/makePerformancePlots.py
@@ -12,7 +12,7 @@ from helpers import *
 from datetime import datetime
 from trainingDict import *
 from tdrstyle import *
-import CMS_lumi as CMS_lumi
+from CMS_lumi import *
 from efficiencyPlots import *
 from occupancyPlots import *
 from resolutionPlots import *
@@ -32,13 +32,10 @@ if __name__ == '__main__':
   parser.add_option("--res1DvsEta", dest="res1DvsEta", action="store_true", default = False)
   parser.add_option("--res2D", dest="res2D", action="store_true", default = False)
   parser.add_option('--emtfModes',nargs='+', help='Set EMTF modes',
-                    choices=[15,14,13,11,7,12,10,9,6,5,3],
                     default = [15,14,13,11,7,12,10,9,6,5,3])
   parser.add_option('--emtfVersions',nargs='+', help='Set EMTF versions',
                     choices=['Run2','Run3_V1p0','Run3_V1p1','Run3_V1p2'],
                     default = ['Run2','Run3_V1p0','Run3_V1p1','Run3_V1p2'])
-  parser.add_option('--emtfColors',nargs='+', help='Set EMTF colors',
-                    default = [kBlue, kRed, kGreen+2, kBlack])
   parser.add_option('--etaMins',nargs='+', help='Set eta minima',
                     default = [1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 1.25])
   parser.add_option('--etaMaxs',nargs='+', help='Set eta maxima',
@@ -46,55 +43,65 @@ if __name__ == '__main__':
   parser.add_option('--emtfPtCuts',nargs='+', help='Set pT cuts',
                     default = [5, 7, 10, 12, 15, 20, 22])
   parser.add_option("--treeName", dest="treeName", action="store", default = "f_logPtTarg_invPtWgt/TestTree")
+  parser.add_option("--verbosity", dest="verbosity", action="store", default = 1, type=int)
   parser.add_option("--etaSlices", dest="etaSlices", action="store_true", default = False)
 
   (options, args) = parser.parse_args()
-
-  print(colored("Configuring:", 'blue'))
 
   ## Run in quiet mode
   if options.batchMode:
     sys.argv.append('-b')
     gROOT.SetBatch(1)
 
-  ## all data files have the same tree name
-  print(colored("- Using Tree name: {}".format(options.treeName), 'blue'))
+  if options.verbosity >= 1:
+    print(colored("Configuring:", 'blue'))
+
+    ## all data files have the same tree name
+    print(colored("- Using Tree name: {}".format(options.treeName), 'blue'))
 
   ## default output directory takes a date and time
   currentDateTime = datetime.now().strftime("%Y%m%d_%H%M%S")
   plotDir = "plots_{}_TEST/".format(currentDateTime)
-  print(colored("- Using output directory {}".format(plotDir), 'blue'))
+  if options.verbosity >= 1:
+    print(colored("- Using output directory {}".format(plotDir), 'blue'))
 
-  ## EMTF modes
-  print(colored("- Analyzing EMTF modes {}".format(options.emtfModes), 'blue'))
+    ## EMTF modes
+    print(colored("- Analyzing EMTF modes {}".format(options.emtfModes), 'blue'))
+    for p in options.emtfModes:
+      if not p in [15,14,13,11,7,12,10,9,6,5,3]:
+        sys.exit("Error: Invalid EMTF mode chosen {}".format(p))
 
-  ## EMTF versions
-  print(colored("- Analyzing EMTF versions {}".format(options.emtfVersions), 'blue'))
+    ## EMTF versions
+    print(colored("- Analyzing EMTF versions {}".format(options.emtfVersions), 'blue'))
 
-  ## EMTF pT thresholds
-  print(colored("- Analyzing EMTF pT thresholds {}".format(options.emtfPtCuts), 'blue'))
+    ## EMTF pT thresholds
+    print(colored("- Analyzing EMTF pT thresholds {}".format(options.emtfPtCuts), 'blue'))
 
-  ## eta minima and maxima
-  print(colored("- Analyzing eta slices with minima {}".format(options.etaMins), 'blue'))
-  print(colored("- Analyzing eta slices with maxima {}".format(options.etaMaxs), 'blue'))
+    ## eta minima and maxima
+    print(colored("- Analyzing eta slices with minima {}".format(options.etaMins), 'blue'))
+    print(colored("- Analyzing eta slices with maxima {}".format(options.etaMaxs), 'blue'))
 
   ## Set proper ROOT style for plots...
   iPeriod = 0
   iPos = 0
   if( iPos==0 ): CMS_lumi.relPosX = 0.12
   setTDRStyle()
-  print(colored("- Setting TDR style for plots\n", 'blue'))
+
+  if options.verbosity >= 1:
+    print(colored("- Setting TDR style for plots\n", 'blue'))
 
   ## get testing data for each mode
   for emtfMode in options.emtfModes:
 
-    print(colored("Analyzing EMTF Mode {}".format(emtfMode), 'blue'))
+    if options.verbosity >= 1:
+      print(colored("Analyzing EMTF Mode {}".format(emtfMode), 'blue'))
 
     ## need option to consider uncompressed or compressed
     trainings = []
     for emtfVersion in options.emtfVersions:
       trainings.append("{}_Mode{}_Compressed".format(emtfVersion, emtfMode))
-    print(colored("{}".format(trainings), 'blue'))
+    if options.verbosity >= 1:
+      print(colored("{}".format(trainings), 'blue'))
 
     eventTrees = []
     for p in trainings:
@@ -108,13 +115,18 @@ if __name__ == '__main__':
 
     ## create a simple helper object to attach attributes to
     class Plotter():
-      def __init__(self, emtfMode, options, eventTrees, legendEntries):
+      def __init__(self, emtfMode, options, eventTrees, legendEntries, plotDir):
         self.options = options
         self.emtfMode = emtfMode
         self.eventTrees = eventTrees
         self.legendEntries = legendEntries
+        self.plotDir = plotDir
+        self.emtfColors = [kBlue, kRed, kGreen+2, kBlack]
+        self.emtfDrawOptions = ["", "same", "same", "same"]
+        self.iPos = iPos
+        self.iPeriod = iPeriod
 
-    myPlotter = Plotter(emtfMode, options, eventTrees, legendEntries)
+    myPlotter = Plotter(emtfMode, options, eventTrees, legendEntries, plotDir)
 
     plotEfficienciesSingleMode(myPlotter)
     #plotResolutionsSingleMode(myPlotter)
