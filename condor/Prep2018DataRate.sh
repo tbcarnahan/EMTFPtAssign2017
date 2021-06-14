2778788d8c0cc1bdb92d51bc742e945d0f16e481
Sven Dildick <sven.dildick@cern.ch>
2021-06-14 11:47:13 -0500

    add v0

--- condor/Prep2018DataRate.sh
+++ condor/Prep2018DataRate.sh
@@ -49,150 +49,200 @@ sleep 60
 
 
 ## Run-3 V1.0
-python submitJobs.py --isRun3Default --emtfMode=15 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=15 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=14 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=14 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=13 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=13 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=11 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=11 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=7 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=7 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=12 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=12 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=10 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=10 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=9 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=9 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=6 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=6 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=5 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=5 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=3 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=3 --jobLabel=Prep2018DataRate
 sleep 60
 
 
 ## Run-3 V1.0 compressed
-python submitJobs.py --isRun3Default --emtfMode=15 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=15 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=14 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=14 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=13 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=13 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=11 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=11 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=7 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=7 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=12 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=12 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=10 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=10 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=9 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=9 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=6 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=6 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=5 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=5 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --emtfMode=3 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --emtfMode=3 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
 
 
 ## Run-3 V1.1
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=15 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=15 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=14 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=14 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=13 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=13 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=11 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=11 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=7 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=7 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=12 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=12 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=10 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=10 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=9 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=9 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=6 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=6 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=5 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=5 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=3 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=3 --jobLabel=Prep2018DataRate
 sleep 60
 
 
 ## Run-3 V1.1 compressed
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=15 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=15 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=14 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=14 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=13 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=13 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=11 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=11 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=7 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=7 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=12 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=12 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=10 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=10 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=9 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=9 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=6 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=6 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=5 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=5 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --emtfMode=3 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --emtfMode=3 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
 
 
 ## Run-3 V1.2
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=15 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=15 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=14 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=14 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=13 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=13 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=11 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=11 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=7 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=7 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=12 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=12 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=10 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=10 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=9 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=9 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=6 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=6 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=5 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=5 --jobLabel=Prep2018DataRate
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=3 --jobLabel=Prep2018DataRate
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=3 --jobLabel=Prep2018DataRate
 sleep 60
 
 
 ## Run-3 V1.2 compressed
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=15 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=15 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=14 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=14 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=13 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=13 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=11 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=11 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=7 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=7 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=12 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=12 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=10 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=10 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=9 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=9 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=6 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=6 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=5 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=5 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
-python submitJobs.py --isRun3Default --useQSBit --useESBit --emtfMode=3 --jobLabel=Prep2018DataRate --useBitComp
+python submitJobs.py --isRun3 --run3Version=v1 --useQSBit --useESBit --emtfMode=3 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+
+
+## Run-3 V0.0
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=15 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=14 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=13 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=11 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=7 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=12 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=10 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=9 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=6 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=5 --jobLabel=Prep2018DataRate
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=3 --jobLabel=Prep2018DataRate
+sleep 60
+
+
+## Run-3 V0.0 compressed
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=15 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=14 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=13 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=11 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=7 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=12 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=10 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=9 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=6 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=5 --jobLabel=Prep2018DataRate --useBitComp
+sleep 60
+python submitJobs.py --isRun3 --run3Version=v0 --emtfMode=3 --jobLabel=Prep2018DataRate --useBitComp
 sleep 60
